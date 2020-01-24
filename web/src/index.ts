0701f1326d5a6a1cd23477bd08bbda01bc78052e
Gustavo Leme <gustavo.leme@webjump.com.br>
2020-01-24 19:16:42 -0300

    adds event listeners mapping

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,6 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Gustavo', age: 26 });
 
+user.set({ age: 999 });
+
 console.log(`
   User Info:
   - Name: ${user.get('name')}
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,10 +1,13 @@
 export interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
+type Callback = () => {};
 
 export class User {
+
+  private subscribers: { [key: string]: Callback[] } = { };
 
   constructor(
     private data: UserProps
@@ -12,6 +15,13 @@ export class User {
 
   get(prop: string): string | number {
     return this.data[prop];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
   }
 
 }
