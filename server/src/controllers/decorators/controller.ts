711ac800de5d99025c503eb24bbcd1b7f26e6a41
Gustavo Leme <gleme@nevada.unr.edu>
2020-02-19 00:15:44 -0300

    adds middlewares and body validation to controller decorator

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+import { bodyValidators } from '../../middlewares/requiredBody';
 
 
 export function controller(pathPrefix: string) {
@@ -12,10 +13,14 @@ export function controller(pathPrefix: string) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata(MetadataKeys.PATH, target.prototype, key);
       const method: Methods = Reflect.getMetadata(MetadataKeys.METHOD, target.prototype, key);
-      
+      const middlewares = Reflect.getMetadata(MetadataKeys.MIDDLEWARE, target.prototype, key) || [];
+      const requiredBodyProps = Reflect.getMetadata(MetadataKeys.VALIDATOR, target.prototype, key) || [];
+      const validator = bodyValidators(requiredBodyProps);
       if (path) {
-        router[method](`${pathPrefix}/${path}`, routeHandler);
+        router[method](`${pathPrefix}/${path}`, ...middlewares, validator, routeHandler);
       }
+
+
     }
   }
 }
