47c6a5b15a10f8ce6c41ab8c85cdba958e4639ac
Gustavo Leme <gleme@nevada.unr.edu>
2020-02-19 00:16:04 -0300

    adds postLogin and logout with decorators

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -1,4 +1,4 @@
-import { controller, get } from './decorators';
+import { controller, get, post, bodyValidator } from './decorators';
 import { Request, Response } from 'express';
 
 @controller('/auth')
@@ -19,6 +19,26 @@ export class LoginController {
     <button>Submit</button>
   </form>
   `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response): void {
+    const { email, password } = req.body;
+  
+    if (email === 'gleme@nevada.unr.edu' && password === 'root') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+  
+    } else {
+      res.status(401).send('Invalid email or password');
+    }
+  }
+
+  @get('/logout')
+  logout(req: Request, res: Response): void {
+    req.session = undefined;
+    res.redirect('/');
   }
   
 }
