df03e434cce11daa993c91174ac4d8646ff38e50
Gustavo Leme <gleme@nevada.unr.edu>
2020-02-19 00:29:50 -0300

    adds PropertyDescriptor interface

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,11 +1,15 @@
 import 'reflect-metadata';
+import { RequestHandler } from 'express';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
 
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 function routeBinder(method: string) {
   return function (path: string) {
-    return function(target: any,  key: string, desc: PropertyDescriptor) {
+    return function(target: any,  key: string, desc: RouteHandlerDescriptor) {
       Reflect.defineMetadata(MetadataKeys.PATH, path, target, key);
       Reflect.defineMetadata(MetadataKeys.METHOD, method, target, key);
     };
